0f2ae4058f60676efb877af9b36483d0e402659a
Pawel MacOS <paweljankowski246@gmail.com>
2019-04-18 16:48:46 +0200

    Add save/load

--- hangman.rb
+++ hangman.rb
@@ -1,9 +1,13 @@
 load "colors.rb"
+require "yaml"
 
 class Game
-  attr_accessor :word
+  attr_accessor :word, :display_word, :guessed_letters, :wrong_guesses, :ww
   def initialize()
     @word = choose_word()
+    @display_word = ''
+    @guessed_letters = []
+    @ww = false
   end
 
   def choose_word()
@@ -16,14 +20,34 @@ class Game
     self.word = s_word.downcase
   end
 
+  def save_game()
+    obj_to_save = self
+    File.open("save.yml", "w") { |file| file.write(obj_to_save.to_yaml) }
+  end
+
+  def load_game()
+    obj_to_load = YAML.load(File.read("save.yml"))
+    self.guessed_letters = obj_to_load.guessed_letters
+    self.word = obj_to_load.word
+    self.display_word = obj_to_load.display_word
+    self.wrong_guesses = obj_to_load.wrong_guesses
+  end
+
   def get_guess(guessed_letters)
-    puts "What is your guess?"
+    puts "What is your guess? (You can also '$save' or '$load' your game"
     x = gets.chomp
     if x.length == 1 && !guessed_letters.include?(x)
       return x
     elsif guessed_letters.include?(x)
       puts "You already guessed that correctly."
       get_guess(guessed_letters)
+    elsif x == "$save"
+      save_game()
+      return x
+    elsif x == "$load"
+      load_game()
+      self.play_game(true)
+      self.ww = true
     else
       puts "Wrong input. Please type in only one character."
       get_guess(guessed_letters)
@@ -48,35 +72,40 @@ class Game
     gets
   end
 
-  def play_game()
-    introduction()
+  def play_game(is_loaded = false)
+    !is_loaded ? introduction() : true
     is_game_won = false
-    display_word = "_" * (self.word.length-2)
+    !is_loaded ? self.display_word = "_" * (self.word.length-2) : true
     round_count = 5
-    i = 0
-    guessed_letters = []
-    while i < round_count && !is_game_won
+    !is_loaded ? self.wrong_guesses = 0 : true
+    !is_loaded ? self.guessed_letters = [] : true
+    while self.wrong_guesses < round_count && !is_game_won
       is_guess_correct = false
       print "\nCorrectly guessed letters: ".italic
-      put_array(guessed_letters)
-      puts "\nIncorrect guesses: #{i}/#{round_count}\n".reverse_color
-      puts display_word.bold
+      put_array(self.guessed_letters)
+      puts "\nIncorrect guesses: #{self.wrong_guesses}/#{round_count}\n".reverse_color
+      puts self.display_word.bold
       puts "\n\n"
-      guessed_char = self.get_guess(guessed_letters)
+      guessed_char = self.get_guess(self.guessed_letters)
+      if guessed_char ==  "$save" || guessed_char == "$load" || !defined?(guessed_char)
+        return
+      end
       self.word.each_char.with_index do |w_char, w_index|
         if guessed_char == w_char
-          display_word[w_index] = guessed_char
-          guessed_letters << guessed_char
+          self.display_word[w_index] = guessed_char
+          self.guessed_letters << guessed_char
           is_guess_correct = true        
         end
       end
-      is_guess_correct ? puts("\nCorrect letter!\n".green) : puts("\nWrong letter\n".red)
-      display_word.include?("_") ? is_game_won = false : is_game_won = true
-      is_guess_correct ? next : i += 1
+      if defined?(guessed_char) && self.ww == false
+        is_guess_correct ? puts("\nCorrect letter!\n".green) : puts("\nWrong letter\n".red)
+      end
+        self.display_word.include?("_") ? is_game_won = false : is_game_won = true
+      is_guess_correct ? next : self.wrong_guesses += 1
     end
-
-    is_game_won ? puts("YOU WON".bg_green) : puts("YOU LOST. The word was ".bg_red + "#{self.word.chomp}".blink.reverse_color)
-    
+    if is_loaded || self.ww == false
+      is_game_won ? puts("YOU WON. The word was ".bg_green + "#{self.word.chomp}".blink.reverse_color) : puts("YOU LOST. The word was ".bg_red + "#{self.word.chomp}".blink.reverse_color)
+    end
   end
 
 end
@@ -84,6 +113,3 @@ end
 x = Game.new()
 puts x.word
 x.play_game()
-
-
-#to save file, i, guessed_letters, display_word, word, 
